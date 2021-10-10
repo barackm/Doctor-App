cb74e4fe3deb051917fa969e83e509717fbdd48a
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-10-11 01:56:16 +0200

    Add emergency button

--- app/screens/tests/TestHistoryScreen.jsx
+++ app/screens/tests/TestHistoryScreen.jsx
@@ -1,5 +1,13 @@
 import React, { useEffect } from 'react';
-import { FlatList, StyleSheet, Text, View, Alert } from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import colors from '../../config/colors';
 import { connect } from 'react-redux';
 import Screen from '../../components/Screen';
@@ -51,6 +59,12 @@ const TestHistoryScreen = ({ tests, loadTests, loading, removeTest }) => {
           </View>
         )}
       />
+      <TouchableOpacity
+        onPress={() => console.log('emergency...')}
+        style={styles.emergencyBtnContainer}
+      >
+        <MaterialIcons name="report-problem" size={30} color={colors.white} />
+      </TouchableOpacity>
     </Screen>
   );
 };
@@ -61,21 +75,37 @@ const styles = StyleSheet.create({
     width: '100%',
     backgroundColor: colors.light,
     position: 'relative',
-    paddingBottom: 10,
+    paddingTop: 5,
   },
-  list: {
-    flex: 1,
-  },
+  list: {},
   noTestsContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1,
     paddingTop: 50,
   },
   noTest: {
     fontSize: 18,
     fontWeight: '500',
     color: colors.medium,
+  },
+  emergencyBtnContainer: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: colors.danger,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: colors.primary,
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 2.84,
+    elevation: 6,
   },
 });
 
